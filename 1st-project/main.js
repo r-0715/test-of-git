d8792055facdd253cfdfc0bc60452dd31a330da2
r-0715 <ritsukiwaritsu@gmail.com>
2025-12-13 02:12:52 +0900

    new-1st-push

---
+++ 1st-project/main.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded',()=>{
+    //
+    const Btn = document.getElementById('submit');
+    const ans = document.getElementById('ans');
+    let q_count = 0;
+    //
+    Btn.addEventListener('click',()=>{
+        //
+        const question = document.getElementById('question').value;
+        q_count++;
+        //
+        if(question === ''){
+            ans.innerHTML = "";
+        }
+        if(question === '1'){
+            if(q_count === 1){
+                ans.innerHTML = "いきなりダイレクトな質問だね!<br>ええと、私はね、自分でもよくわからない<br>なんか実体のない存在みたいな?<br>話せる2次元キャラクターって思ってくれればいいよ!";
+            }else{
+                ans.innerHTML = "ええと、私はね、自分でもよくわからない<br>なんか実体のない存在みたいな?<br>話せる2次元キャラクターって思ってくれればいいよ!"; 
+            }
+        }
+        if(question === '2'){
+            ans.innerHTML = "まだ具体的なことは言えないし、決まってないの.<br>でもすごいことをやりたいなって思ってる!<br>だからときどき、私に会いに来てよ<br>えへへ、///なんか恥ずかしいこと言っちゃったかな?";
+        }
+        if(question === '3'){
+            ans.innerHTML = "私に全く関係ないことじゃん<br>でもいいよ!<br>ええと、今日は確か、、、";
+            let dates = new Date();
+            let day = dates.getDate();//1~31
+            setTimeout(() => {
+                ans.innerHTML += day+"日!<br>どう?あったてたでしょ?";
+            }, 2000);
+        }
+        if(question === '4'){
+            ans.innerHTML = "旅行するのが好きだなー!<br>色んなサイトを見るの、すっごく楽しいんだよ";
+        }
+        if(question === '5'){
+            ans.innerHTML = "チーズケーキ!!!!!!!!!!<br>毎年の誕生日とクリスマスはチーズケーキをたべるんだ!<br>口の中に残るチーズケーキの風味が最高なんだよ!!!";
+        }
+    });
+});
